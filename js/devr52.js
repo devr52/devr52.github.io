961cfbdf4eae3f7aa82ac9b1e16a6bd2097adb12
dughot <ed.vnueva@gmail.com>
2016-10-17 21:24:54 +0800

    minor adjustments

--- js/devr52.js
+++ js/devr52.js
@@ -21,10 +21,4 @@
         }
     })
 
-    $('[data-toggle="tooltip"]').tooltip();
-    $("a[href^=#]").on("click", function (e) {
-        e.preventDefault();
-        history.pushState({}, "", this.href);
-    });
-
 }(jQuery);
